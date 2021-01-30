0e71e8912bbb2dc245ee7d80b202747d34014bb8
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-30 13:33:42 +0000

    user can login to Chitter

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
+    enable :sessions
   get '/' do
     'Hello World'
+    erb(:index)
   end
 
+  post '/signup' do
+    session[:username_login] = params[:username]
+   redirect '/logged_in'
+  end
+
+  get '/logged_in' do
+    @username = session[:username_login]
+    erb(:logged_in)
+  end
   run! if app_file == $0
 end
